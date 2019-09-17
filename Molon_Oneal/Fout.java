cd90b8a15839ae8915f1cf76f2377fd1d20b5be2
Jc Mascarinas <royalemascarinas@gmail.com>
2019-09-17 09:37:47 +0800

    commit as oneal molon

---
+++ Molon_Oneal/Fout.java
@@ -0,0 +1,24 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Fout {
+
+    public static void main (String[]args)throws IOException{
+    
+    	System.out.print("Enter the file name with extension:");
+    	Scanner sc = new Scanner(System.in);
+    	
+    	File MolonFile = new File(sc.nextLine()+".txt");
+    	sc = new Scanner(MolonFile);
+    	
+    	String MolonLine = sc.nextLine();
+    	System.out.println(MolonLine);
+    	
+    	sc.close();
+    }
+    
+    
+}
